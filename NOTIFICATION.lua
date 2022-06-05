dd197afc74e211ae4ebc96d004d31726c7a3adb3
heap <68669985+heap-v@users.noreply.github.com>
2022-06-05 18:15:20 +0100

    Create NOTIFICATION.lua

---
+++ NOTIFICATION.lua
@@ -0,0 +1,8 @@
+game.StarterGui:SetCore("SendNotification", {
+Title = "heap";
+Text = "Injected!";
+Button1 = "Love you daddy.";
+Icon = "http://www.roblox.com/asset/?id=5107182098";
+Duration = 120;
+Callback = NotificationBindableFunction;
+})
